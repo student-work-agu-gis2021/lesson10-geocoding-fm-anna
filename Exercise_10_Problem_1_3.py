5007ba0e3b5fb257c029cb66b70f2ce124562661
fmanna <81938762+fm-anna@users.noreply.github.com>
2021-07-02 06:38:06 +0000

    I read shopping_centers.txt that you just created into a pandas DataFrame called data.

--- Exercise_10_Problem_1_3.py
+++ Exercise_10_Problem_1_3.py
@@ -15,6 +15,7 @@ data = None
 data = pd.read_table('shopping_centers.txt',sep = ';', header=None)
 data.index.name = 'id'
 data.columns = ['name','addr']
+
 #TEST COEE
 # Check your input data
 print(data)
